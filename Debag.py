18a3848eb2711b11fb31d130679c6ef04e4f64b5
QAFed <143259597+QAFed@users.noreply.github.com>
2024-07-07 20:37:12 +0300

    add diff in reg_adm_ev assert

--- Debag.py
+++ Debag.py
@@ -1,13 +1,13 @@
 
-# from endpoints.reg_adm_event import RegAdmEvent
-#
-# import T_DATA
-#
-# reg_adm_ev = RegAdmEvent()
-# reg_adm_ev.reg_adm_event(T_DATA.FIRST_ADM_EVENT)
-# print(reg_adm_ev.response, reg_adm_ev.response_json)
+from endpoints.reg_adm_event import RegAdmEvent
+
+import T_DATA
+
+reg_adm_ev = RegAdmEvent()
+reg_adm_ev.reg_adm_event(T_DATA.FIRST_ADM_EVENT)
+reg_adm_ev.assert_response_body()
 
 
-from endpoints.clear_db import ClearDB
-cdb = ClearDB()
-cdb.clear_db()
+# from endpoints.clear_db import ClearDB
+# cdb = ClearDB()
+# cdb.clear_db()
--- endpoints/reg_adm_event.py
+++ endpoints/reg_adm_event.py
@@ -1,12 +1,26 @@
 import requests
 import LINKS
+from deepdiff import DeepDiff
+
 
 class RegAdmEvent:
     def __init__(self):
         self.end_point = "/journal/admin"
         self.response = None
         self.response_json = None
+        self.mod_response = None
+        self.id = None
+        self.payload = None
 
     def reg_adm_event(self, payload):
-        self.response = requests.put(LINKS.TARGET_HOST+self.end_point,json=payload)
+        self.response = requests.put(LINKS.TARGET_HOST+self.end_point, json=payload)
         self.response_json = self.response.json()
+        self.payload = payload
+
+    def assert_response_body(self):
+        self.mod_response = self.response.json()
+        self.id = self.mod_response.pop('id')
+        diff = DeepDiff(self.payload, self.mod_response, ignore_order=True)
+        if diff:
+            raise AssertionError(diff)
+        assert not diff
